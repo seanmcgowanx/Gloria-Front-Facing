286fe9faefbed8c553fa0076fc9e37ac21dcd9a7
seanmcgowanx <162648241+seanmcgowanx@users.noreply.github.com>
2025-01-26 13:02:08 -0700

    menu
    -fixed issue with menu re fixed position of logo

--- src/global.js
+++ src/global.js
@@ -4,14 +4,17 @@ const menu = document.getElementById('menu');
 const questions = document.querySelectorAll(".question");
 const menuItems = document.querySelectorAll(".menu-item");
 const logo = document.getElementById('logo');
+const header = document.querySelector('header');
+const heroHeader = document.querySelector('.hero-header')
 
 
 // Event Listeners
 
 menuButton.addEventListener('click', () => {
   menu.classList.toggle('visible'); // Toggle Menu
-  logo.classList.toggle('fixed');  // Toggle fixed logo in Menu
+  logo.classList.toggle('fixed'); // Toggle fixed position of logo
 });
+
 
 menuItems.forEach((item) => {
     item.addEventListener("click", () => {
@@ -22,7 +25,7 @@ menuItems.forEach((item) => {
 document.addEventListener('click', (event) => {
     if (menu.classList.contains('visible') && isClickOutsideMenu(event)) {
       menu.classList.remove('visible'); // Close menu when clicking outside
-      logo.classList.remove('fixed');  // Reset logo position
+      logo.classList.remove('fixed'); // Remove fixed logo when clicked outside
     }
   });
 
